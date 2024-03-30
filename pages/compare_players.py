07260f2eb804dc01e537a6fe84a80131401cefb7
brutucas <benlucas6@gmail.com>
2024-03-30 11:15:06 +0100

    Updated code, graph images, screenshots

---
+++ pages/compare_players.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import plotly.graph_obejects as go
+import plotly.express as px
+import streamlit as st
+
---
+++ pages/identify_outliers.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import plotly_graph_objects as go
+import plotly.express as px
+import streamlit as st
